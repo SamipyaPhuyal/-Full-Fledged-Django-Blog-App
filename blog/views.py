27bd6b07f73fdea17d6e6a916cc65a3c3c1fbf20
SamipyaPhuyal <samipyaphuyal012@gmail.com>
2026-03-05 21:15:16 +0545

    created user post CRUD ability

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-from django.views.generic import ListView,DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin,UserPassesTestMixin
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 # Create your views here.
 
 def home(request):
@@ -17,8 +18,41 @@ class PostDetailView(DetailView):
    model=Post
    ordering=["-time"]
 
+class PostCreateView(LoginRequiredMixin,CreateView):
+   model=Post
+   fields=["title","content"] 
+   
+   def form_valid(self, form):
+       form.instance.author=self.request.user
+       return super().form_valid(form)
+   
+class PostUpdateView(LoginRequiredMixin,UpdateView,UserPassesTestMixin):
+   model=Post
+   fields=["title","content"] 
+   
+   def form_valid(self, form):
+       form.instance.author=self.request.user
+       return super().form_valid(form)
+   
+   def test_func(self):
+      post=self.get_object()
+      if self.request.user == post.author:
+         return True
+      return False
+   
 def about(request):
    return render(request,"blog/about.html")
+
+class PostDeleteView(LoginRequiredMixin,DeleteView,UserPassesTestMixin):
+   model=Post
+   success_url='/'
+   
+   def test_func(self):
+      post=self.get_object()
+      if self.request.user == post.author:
+         return True
+      return False
+   
 
    
 
